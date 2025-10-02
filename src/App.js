5316e7ad0701132593501d080fe04aaf748d8ae5
Ntebogeng Marobela <officialntebo@gmail.com>
2025-10-02 21:02:49 +0200

    added inital files for header layout

--- src/App.js
+++ src/App.js
@@ -5,6 +5,20 @@ const App =() => {
   return (
     <div className="App">
       <h1>AirBnb Clone</h1>
+      {/* 
+      Home Page
+       Header
+
+       Banner
+        Search
+      
+      Card 
+
+      Footer
+
+      Search Page
+       Header
+      */}
     </div>
   );
 }
