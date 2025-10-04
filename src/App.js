a0bc6fc4efc9ecbe45195ae423f3f5d643e0fdfe
Ntebogeng Marobela <officialntebo@gmail.com>
2025-10-04 03:23:56 +0200

    added footer to layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-
 import './App.css';
+import Header from "./components/layout/Header";
+import Home from './components/Home';
+import Footer from "./components/layout/Footer"
 
 const App =() => {
   return (
     <div className="App">
-      <h1>AirBnb Clone</h1>
+      <Header />
+      <Home />
+      <Footer />
       {/* 
       Home Page
        Header
